a91176b2b793322b34145a4d9ae1c955ee414d58
Adrián Tejedor <adriantejedor96@gmail.com>
2018-08-20 11:44:31 +0200

    FC
    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
+import { SharedModule } from './shared.module';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import { MODULES, PROVIDERS } from './app.imports';
 
 @NgModule({
   declarations: [
+    // App Core
     MyApp,
-    HomePage
   ],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
+    MODULES,
+    IonicModule.forRoot(MyApp),
+    SharedModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
   ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+  providers: [PROVIDERS, { provide: ErrorHandler, useClass: IonicErrorHandler }]
 })
-export class AppModule {}
+export class AppModule { }
